8d432c3bed3c0bd96b3384f91b939c13275f9ccd
Antonio Zanon <antonio.zanon99@gmail.com>
2025-07-09 19:18:29 -0300

    Alteração de localhost para o site

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-// Rota necessária para Sanctum emitir cookies corretamente
 Route::middleware(['web'])->get('/sanctum/csrf-cookie', function (Request $request) {
     return response()->json(['message' => 'CSRF cookie set']);
 });
 
-// Rota genérica para SPA do React (vem por último!)
-Route::middleware(['web'])->get('/{any}', function () {
+// Todas as rotas que não forem encontradas acima vão servir o index.html do React
+Route::get('/{any}', function () {
     return file_get_contents(public_path('index.html'));
 })->where('any', '.*');
